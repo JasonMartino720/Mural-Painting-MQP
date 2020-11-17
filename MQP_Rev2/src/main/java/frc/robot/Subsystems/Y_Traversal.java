78d733cb165834fb597e57b8dc18d8e9798493f8
JasonMartino720 <jasonamartino@gmail.com>
2020-11-16 22:18:37 -0500

    Update Y_Traversal.java

--- MQP_Rev2/src/main/java/frc/robot/Subsystems/Y_Traversal.java
+++ MQP_Rev2/src/main/java/frc/robot/Subsystems/Y_Traversal.java
@@ -40,8 +40,7 @@ public class Y_Traversal extends SubsystemBase {
   }
 
   public double getEncPosition() {
-    //return this.EncY.getDistance();
-    return this.EncY.get(); 
+    return this.EncY.getDistance();
   }
 
   public void resetEnc(){
