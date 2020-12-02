1da5b19d7a03a91c5008b0684342678557d52e7c
mhoff2016 <48568665+mhoff2016@users.noreply.github.com>
2020-12-02 15:20:57 -0500

    Update Constants.java

--- MQP_Rev2/src/main/java/frc/robot/Subsystems/Constants.java
+++ MQP_Rev2/src/main/java/frc/robot/Subsystems/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
     public static final int k_EncXPort1 = 4;
     public static final int k_EncXPort2 = 5;
     // Encoder conversion factor in inches per pulse
-    public static final float k_EncXConversion = 0.0290358901f;
+    public static final float k_EncXConversion = 0.4645742417f;
     // Encoder min rate in X direction
     public static final double k_EncXMinRate = 0.125;
     // Encoder is reversed or not
