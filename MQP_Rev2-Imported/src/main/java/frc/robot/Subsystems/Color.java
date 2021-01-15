b56d692765758e5dce8707b89c40534ccde800b5
mhoff2016 <48568665+mhoff2016@users.noreply.github.com>
2021-01-15 17:44:12 -0500

    updated code to 2021

---
+++ MQP_Rev2-Imported/src/main/java/frc/robot/Subsystems/Color.java
@@ -0,0 +1,74 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Subsystems;
+
+/**
+ * Add your docs here.
+ */
+public enum Color{
+    NONE(0),
+    ORANGE(1), 
+    BLUE(2), 
+    WHITE(3),
+    BLACK(4), 
+    RED(5), 
+    YELLOW(6), 
+    WHITE2(7), 
+    PURPLE(8);
+
+    protected final int colorVal;
+
+    Color(int colorVal){
+        this.colorVal = colorVal;
+    }
+
+    private Color retColor;
+
+    public Color set(int value){
+        switch(value){
+            case 1:
+               retColor = Color.ORANGE;
+            break;
+
+            case 2:
+               retColor = Color.BLUE;
+            break;
+
+            case 3:
+               retColor = Color.WHITE;
+            break;
+
+            case 4:
+               retColor = Color.BLACK;
+            break;
+
+            case 5:
+               retColor = Color.RED;
+            break;
+
+            case 6:
+               retColor = Color.YELLOW;
+            break;
+
+            case 7:
+               retColor = Color.WHITE2;
+            break;
+
+            case 8:
+               retColor = Color.PURPLE;
+            break;
+
+            default:
+                retColor = Color.NONE;
+            break;
+        }
+        return retColor;
+    }
+
+
+}
