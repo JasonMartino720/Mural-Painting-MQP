59315f51ffad9d5815e2796d252dc7f833fec82b
mhoff2016 <48568665+mhoff2016@users.noreply.github.com>
2021-01-26 13:52:03 -0500

    fixed importing to 2021

---
+++ MQP_Rev2-Imported/src/main/java/frc/robot/Subsystems/Brush.java
@@ -0,0 +1,257 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Subsystems;
+
+import javax.lang.model.util.ElementScanner6;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import java.lang.Math.*;
+public class Brush extends SubsystemBase {
+  Timer brushTimer = new Timer();
+  Spark m_paintSelector = new Spark(Constants.k_PaintSelectorPort);
+  VictorSP m_paintTrigger = new VictorSP(Constants.k_PaintTriggerPort);
+  //DigitalInput paintSelctorLimit = new DigitalInput(Constants.k_PaintSelectorLimitPort);
+  DigitalInput paintTriggerBtn = new DigitalInput(Constants.k_PaintTriggerBtnPort);
+  public final Encoder EncPaint = new Encoder(Constants.k_EncPaint1, Constants.k_EncPaint2);
+  /**
+   * Creates a new Brush.
+   */
+  public Brush() {
+    brushState = BrushState.INIT;
+    EncPaint.setDistancePerPulse(Constants.k_EncPaintConversion);
+    EncPaint.setReverseDirection(true);
+  }
+
+  public int currentColor, nextColor;
+  private double paintStartTime, lastTime, resetStartTime, waitStartTime, waitTime;
+  private boolean isDoneSelecting, countUp, lastSwitchState;
+  public static boolean finishedPainting = false;
+
+  private enum BrushState {
+    INIT, IDLE, PAINTING, SELECTING_COLOR, WAIT_FOR_COLOR, WAIT_FOR_PAINT, WAIT_FOR_RESET, WAIT_FOR_TIME
+  }
+
+  private BrushState brushState, nextBrushState;
+
+  public void init(){
+    //Any initialization that may need to be repeated, ie. should not be called in constructor
+    brushState = BrushState.INIT;
+  }
+
+  // Spin Paint Selector CW at Constant Speed
+  public void spinSelectorCW() {
+    m_paintSelector.set(Constants.SELECTOR_CW_SPEED);
+  }
+
+  // Spin Paint Selector CCW at Constant Speed
+  public void spinSelectorCCW() {
+    m_paintSelector.set(Constants.SELECTOR_CCW_SPEED);
+  }
+
+  // Spin Paint Selector CCW at Constant Speed
+  public void spinSelectorOff() {
+    m_paintSelector.set(0.0);
+  }
+
+  // Checks Limit Switch to see if Geneva Mechanism is aligned
+  /*public boolean getSelectorSwitch() {
+    return !paintSelctorLimit.get();
+  }*/
+
+  // Checks Limit Switch to see if trigger is l;ined up correctly
+  public boolean getTriggerBtn() {
+    return !paintTriggerBtn.get();
+  }
+
+  public void paintForTime() {
+    m_paintTrigger.set(Constants.k_PaintTriggerSpeed);
+  }
+
+  public void triggerReset() {
+    m_paintTrigger.set(-Constants.k_PaintTriggerSpeed);
+  }
+  public void stopPainting() {
+    m_paintTrigger.set(0.0);
+  }
+
+  public double getEncPosition(){
+    return this.EncPaint.getDistance();
+  }
+
+  public void resetEnc(){
+    EncPaint.reset();
+  }
+  public boolean atPosition(int position){
+    if(Math.abs(position - getEncPosition()) < Constants.k_TolerancePaint){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public void update(Color previousColor, Color color, boolean readyToPaint) {
+    //System.out.println("previous color" + previousColor + "current color: " + color);
+    switch (this.brushState) {
+    
+    case INIT:
+      brushTimer.start();
+      if(this.currentColor != color.colorVal){
+        this.currentColor = previousColor.colorVal;
+      }
+      System.out.println("init");
+      brushState = BrushState.IDLE;
+      this.lastSwitchState = true;
+    break;
+  
+    case IDLE:
+
+      if(Brush.finishedPainting){
+        Brush.finishedPainting = false;
+      }
+
+      if (color.colorVal == this.currentColor && readyToPaint){
+        brushState = BrushState.WAIT_FOR_TIME;
+        this.waitStartTime = brushTimer.get();
+        this.waitTime = 0.25;
+        nextBrushState = BrushState.PAINTING;
+      }
+      else if (color.colorVal != this.currentColor && color.colorVal != 0){
+        brushState = BrushState.SELECTING_COLOR;
+      }    
+      else
+        brushState = BrushState.IDLE;
+        // if(color.colorVal == this.currentColor){
+        //   System.out.println("Correct Color, Waiting for X/Y");
+        // }
+
+        // if(readyToPaint){
+        //   System.out.println("Correct Pos, Waiting for Selector");
+        // }
+
+    break;
+
+    //Triggers the paint can and records the start time
+    //Sets state to WAIT_FOR_PAINT when finished   
+    case PAINTING:
+      Brush.finishedPainting = false;    
+      paintStartTime = brushTimer.get();
+      this.paintForTime();
+      brushState = BrushState.WAIT_FOR_PAINT;
+      //System.out.println("BEGIN PAINTING PROCEDURE");
+    break;
+    
+    //Starts the color selector moving the ideal direction
+    //Sets state to WAIT_FOR_COLOR when finished 
+    case SELECTING_COLOR:
+      //Optimization to decide whether to turn right or left, pretty cool how simple it is
+      //System.out.println("current color: " + this.currentColor + " desired color: " + color.colorVal);
+      if(this.currentColor <= 4){
+        if(color.colorVal <= (this.currentColor + 4) && color.colorVal > this.currentColor){
+          this.spinSelectorCCW();
+          //System.out.println("ccw");
+          countUp = true; //If turning counter-clockwise then increment downwards at UPDATE
+          brushState = BrushState.WAIT_FOR_COLOR;
+        }
+        else{
+          this.spinSelectorCW();
+          //System.out.println("cw");
+          countUp = false; //If turning clockwise then increment upwards at UPDATE
+          brushState = BrushState.WAIT_FOR_COLOR;
+        }
+      }
+      else{
+        if(this.currentColor > color.colorVal && (this.currentColor - 3) <= color.colorVal){
+          this.spinSelectorCW();
+          //System.out.println("cw");
+          countUp = false; //If turning clockwise then increment upwards at UPDATE
+          brushState = BrushState.WAIT_FOR_COLOR;
+        }
+        else{
+          this.spinSelectorCCW();
+          //System.out.println("ccw");
+          countUp = true; //If turning counter-clockwise then increment downwards at UPDATE
+          brushState = BrushState.WAIT_FOR_COLOR;
+        }
+      }
+    break;
+
+
+    //Waits for Paint Selector to reach desired color
+    case WAIT_FOR_COLOR:
+      // System.out.println("current color wait for color" + this.currentColor);
+      if(Math.abs(this.getEncPosition()) > 0.975 && Math.abs(this.getEncPosition()) < 1.025){
+        this.resetEnc();
+        //Positive
+        if(countUp){
+          if(this.currentColor + 1 > 8){
+            this.currentColor = 1;
+          }
+          else{
+            this.currentColor += 1;
+          }
+        }
+        //Negative
+        else{
+          if(this.currentColor - 1 < 1){
+            this.currentColor = 8;
+          }
+          else{
+            this.currentColor -= 1;
+          }
+        }
+        //System.out.println("Last Switch State " + this.lastSwitchState);
+      }
+
+      if(this.currentColor == color.colorVal){
+        this.spinSelectorOff();
+        brushState = BrushState.IDLE;
+      }
+    break;
+
+    //Waits designated amount of time for paint application to finish,
+    //stops paint trigger motor, and resets the active color to none.
+    //Returns to IDLE when finished
+    case WAIT_FOR_PAINT:
+     // System.out.println("WAITING FOR PAINTING TO FINISH");
+      if(brushTimer.get() - paintStartTime >= Constants.k_PaintingTime){ 
+        this.triggerReset();
+        //TODO: this.currentColor = Color.NONE.colorVal;
+        brushState = BrushState.WAIT_FOR_RESET;
+      }
+    break;
+
+    case WAIT_FOR_RESET:
+      //System.out.println("WAITING FOR RESET TO FINISH");
+      //System.out.println(getTriggerBtn());
+      if(getTriggerBtn()){
+        this.stopPainting();
+
+        //TODO: this.currentColor = Color.NONE.colorVal;
+        brushState = BrushState.IDLE;
+        Brush.finishedPainting = true;
+      }
+    break;
+
+    case WAIT_FOR_TIME:
+      if(brushTimer.get() - waitStartTime > waitTime)
+      {
+        brushState = nextBrushState;
+      }
+    break;  
+    }
+  }
+}
---
+++ MQP_Rev2-Imported/src/main/java/frc/robot/Subsystems/Constants.java
@@ -0,0 +1,93 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Subsystems;
+
+import static edu.wpi.first.wpilibj.SerialPort.Port.kMXP;
+
+import edu.wpi.first.wpilibj.SerialPort;
+
+//TODO: Fill in these ports when known
+public class Constants {
+
+// ----X-Traversal--------------------------------------------------------------
+    // SRX ID
+    public static final int k_XTraversalPort = 0;
+    // X Encoder DIO Pins
+    public static final int k_EncXPort1 = 4;
+    public static final int k_EncXPort2 = 5;
+    // Encoder conversion factor in inches per pulse
+    public static final float k_EncXConversion = 0.24488f;
+    // Encoder min rate in X direction
+    public static final double k_EncXMinRate = 0.125;
+    // Encoder is reversed or not
+    public static final boolean k_EncXReverse = true;
+    // Tolerance
+    public static final double k_ToleranceX = 2000.0;
+
+    //PID Constants
+    public static final int k_IDX = 0;
+    public static final int k_TimeoutMs = 0;
+
+    public static final double k_xP = 3.0;
+    public static final double k_xI = 0.000;
+    public static final double k_xD = 2.50;
+    public static final double k_xF = 0.0; 
+    
+// ----Y-Traversal--------------------------------------------------------------
+    public static final int k_YTraversalPort = 1;
+    // Y ToF Sensor
+    public static final SerialPort.Port k_YToFSerialPort = kMXP;
+    //Y Encoder DIO Pins
+    public static final int k_EncYPort1 = 2;
+    public static final int k_EncYPort2 = 3;
+        //Encoder conversion factor in inches per pulse
+        public static final float k_EncYConversion = 0.00045024f; 
+        //Encoder min rate in X direction
+        public static final double k_EncYMinRate = 0.125; 
+        //Encoder is reversed or not
+        public static final boolean k_EncYReverse = false;
+        //Tolerance
+        public static final double k_ToleranceY = 0.1;
+
+//----PAINT SELECTOR--------------------------------------------------------------
+    //Constant int for Spark Channel
+    public static final int k_EncPaint1 = 7;
+    public static final int k_EncPaint2 = 8;
+        //Port for accompanying limit switch
+        public static final int k_PaintSelectorPort = 1;
+        //Speeds for single turn method
+        //Encoder conversion factor in inches per pulse
+        public static final float k_EncPaintConversion = 0.125f; 
+        //Encoder min rate in X direction
+        public static final double k_EncPaintMinRate = 0.125; 
+        //Encoder is reversed or not
+        public static final boolean k_EncPaintReverse = false;
+        //Tolerance
+        public static final double k_TolerancePaint = 0.1;
+        public static final double SELECTOR_CW_SPEED = 0.65;
+        public static final double SELECTOR_CCW_SPEED = -0.65;
+
+    //Constant int for VexMotor Channel
+    //  Controls Paint Trigger
+    public static final int k_PaintTriggerPort = 2;
+        //Speed for painting 
+        public static final double k_PaintTriggerSpeed = 0.25;
+        //Static times for paint and reset 
+        public static final double k_PaintingTime = 0.28; //In Seconds
+        public static final double k_ResetTime = 0.1; //In Seconds
+        //Paint Trigger Button
+        public static final int k_PaintTriggerBtnPort = 6;
+
+//----VEX BUTTON---------------------------------------------------------------------
+    //Button DIO Port
+    public static final int k_VexBtnPort = 0;
+    
+
+    
+
+}
